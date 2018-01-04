e2af6760beb2be6fdd422c272e31c9bd88906201
Bitterkoekje <piterannema@gmail.com>
2018-01-04 19:44:09 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,157 @@
+# noinspection PyPackageRequirements
+import serial
+import time
+from urllib.request import urlopen
+import json
+
+
+def read_last_line(ser, whitelist: dict) -> dict:
+    """
+    Reads the last line from the serial port (ser) and return the data as a dictionary.
+
+    :param ser: The serial connection
+    :param whitelist: A list of known mac-addresses. If the mac address is not in this list, name is returned as False.
+    :return: A dictionary with mac, rssi, time, name
+    :rtype: dict
+    """
+
+    # THIS IS A DUMMY LAST_LINE FOR DEBUGGING!
+    # ---------------------------------------
+    # macs = ['2c:f0:a2:d8:2e:bf', '2c:f0:23:d8:df:af', 'c0:ee:fb:42:91:99']
+    # data = [np.random.randint(-85, -60), np.random.choice(list(whitelist))]
+    # data = [np.random.randint(-85, -60), np.random.choice(macs)]
+    # data = []
+    # ---------------------------------------
+
+    try:
+        data = ser.readline().decode()[:-2].split(',')
+    except AttributeError:
+        print('Warning, no serial data found')
+        return dict()
+    else:
+        return {'mac': data[1], 'rssi': 100 + data[0], 'time': time.time(), 'name': whitelist.get(data[1], False)}
+
+
+def update(array: dict, last_line: dict) -> dict:
+    """
+    Preforms several checks whether the currentmost data from the serial connection is valid.
+    If this is the case, it adds this entry to the array of present mac-addresses.
+
+    :param array: The array of present mac-addresses
+    :param last_line: The last line read from the serial connection
+    :return: The array of present mac-addresses
+    :rtype: dict
+    """
+    
+    # Storing this first gives a slight performance increase
+    mac = last_line['mac']
+    
+    # Check whether an entry exists for this mac-address.
+    # If not, add an empty entry for this mac-address.
+    # Otherwise, make sure the last known timestamp is more than one second old.
+    if mac not in array:
+        array[mac] = {'data': [], 'name': last_line['name']}
+    elif last_line['time'] - array[mac]['data'][-1]['time'] < 1:
+        return array
+
+    # Add the timestamp to the entry.
+    array[mac]['data'].append({'time': last_line['time'], 'rssi': last_line['rssi']})
+
+    return array
+
+
+def pop_timed_out(array: dict, t: float) -> dict:
+    """
+    Check wether a timestamp in any of the entries is too old. If so, it is popped from data.
+    If this results in an entry without data, the entry is removed.
+
+    :param array: The array of present mac-addresses
+    :param t: The time with which to compare the timestamps.
+    :return: The array that whas provided, minus all popped timestamps.
+    :rtype: dict
+    """
+    for entry in list(array):
+        for i, d in enumerate(array[entry]['data']):
+            if t - d['time'] > 300:
+                array[entry]['data'].pop(i)
+
+        if not array[entry]['data']:
+            del array[entry]
+
+    return array
+
+
+def save_present(array: dict, t: float):
+    """
+    Splits the array of present mac-addresses by known/unknown and adds a line to their respecive log files.
+    Also sends the list of present-known to the online overview using json
+    :param array: The array of present mac-addresses
+    :param t: The time of saving the data.
+    """
+    pr_known = []
+    pr_unknown = []
+    for entry in list(array):
+        if array[entry]['name']:
+            pr_known.append(array[entry]['name'])
+        else:
+            pr_unknown.append(entry)
+
+    pr_known.append(str(t))
+    pr_unknown.append(str(t))
+
+    json_string = json.dumps(pr_known)
+    url = ('https://www.borrelcie.vodka/present?data=' + json_string).replace(' ', '')
+    print('Uploading: ' + url)
+    urlopen(url)
+
+    with open('./present/present', 'a') as present:
+        present.write(str(pr_known) + '\n')
+    print('Nu zijn er:', pr_known)
+
+    with open('./present/unknown', 'a') as present:
+        present.write(str(pr_unknown) + '\n')
+    print('De onbekenden:', pr_unknown)
+
+
+def check_whitelist() -> dict:
+    """
+    Import the whitelist of mac-addresses and return it as a dict.
+    :return: whitelist
+    :rtype: dict
+    """
+
+    with open('./whitelists/whitelist') as text:
+        whitelist = eval(text.read())
+    return whitelist
+
+
+def main():
+    t = time.time()
+    whitelist = check_whitelist()
+    ser = serial.Serial('/dev/ttyUSB0', 115200)
+
+    array = dict()
+    save_pr_time = t
+    check_wl_time = t
+    while True:
+        t = time.time()
+        last_line = read_last_line(ser, whitelist)
+        if not last_line:
+            time.sleep(10)
+        else:
+            array = update(array, last_line)
+
+        # Save the list of present mac-addresses every two seconds
+        if t - save_pr_time > 2:
+            array = pop_timed_out(array, t)
+            save_present(array, t)
+            save_pr_time = t
+
+        # Check whitelist every 30 minutes
+        if t - check_wl_time > 3600:
+            whitelist = check_whitelist()
+            check_wl_time = t
+
+
+if __name__ == '__main__':
+    main()
---
+++ timeline.py
@@ -0,0 +1,144 @@
+import numpy as np
+import time
+import datetime
+import re
+import matplotlib
+import os
+
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
+
+
+def check_schermwhitelist() -> dict:
+    """
+    Import the whitelist of mac-addresses and return it as a dict.
+    :return: whitelist
+    :rtype: dict
+    """
+
+    with open('./whitelists/schermwhitelist') as text:
+        whitelist = eval(text.read())
+    return whitelist
+
+
+def read_data(dt_min: datetime, dt_max: datetime):
+    """
+    Import names and timestamps from the present-logfile and return them as a dict with name:datetimes entries.
+    :param dt_min:  Minimum datetime
+    :param dt_max:  Maximum datetime
+    :return: present
+    :rtype: dict
+    """
+    # Get the data from the present-logfile.
+    # For performace reasons, only the last part of the file is loaded. This only works on UNIX.
+    with os.popen('tail -n 100000 ./present/present') as f:
+        file = f.read()
+
+    # Import the whitelist
+    whitelist = check_schermwhitelist()
+
+    present = dict()
+
+    # Split the data from present by line
+    for line in file.splitlines():
+        # Extract the data from the line, the last element is always the timestamp
+        data = re.findall(r"[' ]([\w\d].*?)['\]]", line)
+
+        # Make sure the line is not only a timestamp
+        if len(data) > 1:
+
+            # Make sure the timestamp is in the specified range
+            dt = datetime.datetime.fromtimestamp(float(data[-1]))
+            if dt_min <= dt <= dt_max:
+
+                # Go through all present names and make sure they are in the whitelist
+                for name in data[:-1]:
+                    if name in whitelist.values():
+
+                        # If there is no entry for this name in the present dict, make one.
+                        if name not in present:
+                            present[name] = []
+
+                        present[name].append(dt)
+
+    return present
+
+
+def saveplot(present: dict, dt_min: datetime, dt_max: datetime, strict_xlim: bool = True):
+    """
+    Save a plot of present names
+    :param present:     Dict of name:datetimes entries
+    :param dt_min:      Minimum datetime
+    :param dt_max:      Maximum datetime
+    :param strict_xlim:
+    """
+
+    fig, ax = plt.subplots(figsize=(16, 9))
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    plt.title(str(dt_min) + ' - ' + str(dt_max))
+
+    # Configure the x-axis such that it takes dates.
+    plt.gcf().autofmt_xdate()
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%y   %H:%M'))
+
+    if dt_max - dt_min <= datetime.timedelta(hours=4):
+        plt.gca().xaxis.set_major_locator(mdates.MinuteLocator(byminute=[0, 15, 30, 45]))
+
+    elif dt_max - dt_min <= datetime.timedelta(hours=24):
+        plt.gca().xaxis.set_major_locator(mdates.HourLocator())
+
+    else:
+
+        plt.gcf().autofmt_xdate(rotation=30, ha='center')
+        plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+        plt.gca().xaxis.set_minor_locator(mdates.HourLocator(byhour=12))
+        plt.gca().xaxis.set_major_formatter(ticker.NullFormatter())
+        plt.gca().xaxis.set_minor_formatter(mdates.DateFormatter('%A\n%d-%m-%y'))
+
+    ax.yaxis.set_ticks_position('left')
+    ax.xaxis.set_ticks_position('bottom')
+
+    # For each name, plot its datetimes.
+    # Each name get's a unique y coordinate.
+    for i, name in enumerate(present):
+        y = np.ones(len(present[name])) * (i + 1)
+        ax.plot(present[name], y, 's', markersize=8)
+
+        # Place the name at the first datetime
+        # plt.text(min(present[name]), i+1.3, name)
+
+    # Use the names as yticks
+    plt.yticks(range(1, len(present) + 1), present)
+
+    if strict_xlim or not present:
+        plt.xlim(dt_min, dt_max)
+
+    plt.ylim(0, len(present) + 1)
+
+    plt.grid()
+    plt.savefig('slide.png', dpi=300)
+    plt.close('all')
+    return
+
+
+def main():
+    while True:
+        dt_max = datetime.datetime.now()
+        dt_min = dt_max - datetime.timedelta(hours=12)
+
+        t = time.time()
+        present = read_data(dt_min, dt_max)
+        saveplot(present, dt_min, dt_max, True)
+
+        print('Plot saved for dates between ' + str(dt_min) + ' and ' + str(dt_max) + ' for ' + str(len(present)) +
+              ' names in ' + str(time.time() - t) + ' seconds.')
+
+        time.sleep(60)
+
+
+if __name__ == '__main__':
+    main()
